d0dcf0a31db8462de338229c3fa7a9d8040c2248
Andy <NebularNerd@users.noreply.github.com>
2022-10-06 13:04:29 +0100

    Two example files that I use for my Admin profiles
    I have two `(Admin)` profiles that I use, rather than having to navigate back to the folder in Admin mode I just use these to pop open an Administrator window in the same location (You cannot add a tab when opening as Admin).

---
+++ cmdhere-a.ps1
@@ -0,0 +1,3 @@
+# Open a Command Prompt tab in Windows Terminal at the same folder location.
+# See https://github.com/NebularNerd/win-term-cmdhere for more
+wt nt -p "Command Prompt (Admin)" -d $pwd
---
+++ pshere-a.ps1
@@ -0,0 +1,3 @@
+# Open a Powershell Admin tab in Windows Terminal at the same folder location.
+# See https://github.com/NebularNerd/win-term-cmdhere for more
+wt nt -p "Windows PowerShell (Admin)" -d $pwd
